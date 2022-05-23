0a834098ba4926b7b601ee540e5e27d3d754dd8f
soCromp <snc40@pitt.edu>
2022-05-22 21:52:37 -0500

    adding captioner code

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # special-octo-pancake
+
+Steps
+1. infer.py generates captions for images and extracts these captions' keywords,
+2. labelkeys.py gets keywords for label names, then determines which caption keywords are spurious and outputs them to spurious.txt and
+3. remspur.py reads in the spurious words, then applies autoLF to the images to black out image patches in which CLIP predicts a spurious word.
+
+Setup
+- Obtain the waterbirds dataset and update DIR in infer.py
+- Download ClipCap checkpoint from https://drive.google.com/file/d/14pXWwB4Zm82rsDdvbGguLfx9F8aM7ovT/view and update model_path in infer.py
---
+++ infer.py
@@ -0,0 +1,243 @@
+# Inference notenook for [CLIP prefix captioning](https://github.com/rmokady/CLIP_prefix_caption/)
+# Adapted from https://github.com/rmokady/CLIP_prefix_caption/blob/main/notebooks/clip_prefix_captioning_inference.ipynb
+# Disclaimer: the authors do not own any rights for the code or data.
+
+######## Settings ########
+# Download model from https://drive.google.com/file/d/14pXWwB4Zm82rsDdvbGguLfx9F8aM7ovT/view
+# and set model_path to its location:
+model_path='./conceptual_weights.pt'
+# Location of the waterbirds images
+DIR = './waterbirds_v1.0'
+
+
+import clip
+import os
+from torch import nn
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn.functional as nnf
+import sys
+from typing import Tuple, List, Union, Optional
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW, get_linear_schedule_with_warmup
+from tqdm import tqdm, trange
+import skimage.io as io
+import PIL.Image
+from string import punctuation
+import spacy
+nlp=spacy.load('en_core_web_md') # if crashes, run python -m spacy download en_core_web_md
+
+
+N = type(None)
+V = np.array
+ARRAY = np.ndarray
+ARRAYS = Union[Tuple[ARRAY, ...], List[ARRAY]]
+VS = Union[Tuple[V, ...], List[V]]
+VN = Union[V, N]
+VNS = Union[VS, N]
+T = torch.Tensor
+TS = Union[Tuple[T, ...], List[T]]
+TN = Optional[T]
+TNS = Union[Tuple[TN, ...], List[TN]]
+TSN = Optional[TS]
+TA = Union[T, ARRAY]
+
+
+D = torch.device
+CPU = torch.device('cpu')
+
+
+def get_device(device_id: int) -> D:
+    if not torch.cuda.is_available():
+        return CPU
+    device_id = min(torch.cuda.device_count() - 1, device_id)
+    return torch.device(f'cuda:{device_id}')
+
+
+CUDA = get_device
+
+current_directory = os.getcwd()
+save_path = os.path.join(os.path.dirname(current_directory), "pretrained_models")
+os.makedirs(save_path, exist_ok=True)
+model_path = os.path.join(save_path, 'model_wieghts.pt')
+
+
+class MLP(nn.Module):
+
+    def forward(self, x: T) -> T:
+        return self.model(x)
+
+    def __init__(self, sizes: Tuple[int, ...], bias=True, act=nn.Tanh):
+        super(MLP, self).__init__()
+        layers = []
+        for i in range(len(sizes) -1):
+            layers.append(nn.Linear(sizes[i], sizes[i + 1], bias=bias))
+            if i < len(sizes) - 2:
+                layers.append(act())
+        self.model = nn.Sequential(*layers)
+
+
+class ClipCaptionModel(nn.Module):
+
+    #@functools.lru_cache #FIXME
+    def get_dummy_token(self, batch_size: int, device: D) -> T:
+        return torch.zeros(batch_size, self.prefix_length, dtype=torch.int64, device=device)
+
+    def forward(self, tokens: T, prefix: T, mask: Optional[T] = None, labels: Optional[T] = None):
+        embedding_text = self.gpt.transformer.wte(tokens)
+        prefix_projections = self.clip_project(prefix).view(-1, self.prefix_length, self.gpt_embedding_size)
+        #print(embedding_text.size()) #torch.Size([5, 67, 768])
+        #print(prefix_projections.size()) #torch.Size([5, 1, 768])
+        embedding_cat = torch.cat((prefix_projections, embedding_text), dim=1)
+        if labels is not None:
+            dummy_token = self.get_dummy_token(tokens.shape[0], tokens.device)
+            labels = torch.cat((dummy_token, tokens), dim=1)
+        out = self.gpt(inputs_embeds=embedding_cat, labels=labels, attention_mask=mask)
+        return out
+
+    def __init__(self, prefix_length: int, prefix_size: int = 512):
+        super(ClipCaptionModel, self).__init__()
+        self.prefix_length = prefix_length
+        self.gpt = GPT2LMHeadModel.from_pretrained('gpt2')
+        self.gpt_embedding_size = self.gpt.transformer.wte.weight.shape[1]
+        if prefix_length > 10:  # not enough memory
+            self.clip_project = nn.Linear(prefix_size, self.gpt_embedding_size * prefix_length)
+        else:
+            self.clip_project = MLP((prefix_size, (self.gpt_embedding_size * prefix_length) // 2, self.gpt_embedding_size * prefix_length))
+
+
+class ClipCaptionPrefix(ClipCaptionModel):
+
+    def parameters(self, recurse: bool = True):
+        return self.clip_project.parameters()
+
+    def train(self, mode: bool = True):
+        super(ClipCaptionPrefix, self).train(mode)
+        self.gpt.eval()
+        return self
+
+
+def generate(
+        model,
+        tokenizer,
+        tokens=None,
+        prompt=None,
+        embed=None,
+        entry_count=1,
+        entry_length=67,  # maximum number of words
+        top_p=0.8,
+        temperature=1.,
+        stop_token: str = '.'):
+    model.eval()
+    generated_num = 0
+    generated_list = []
+    stop_token_index = tokenizer.encode(stop_token)[0]
+    filter_value = -float("Inf")
+    device = next(model.parameters()).device
+
+    with torch.no_grad():
+
+        for entry_idx in trange(entry_count):
+            if embed is not None:
+                generated = embed
+            else:
+                if tokens is None:
+                    tokens = torch.tensor(tokenizer.encode(prompt))
+                    tokens = tokens.unsqueeze(0).to(device)
+
+                generated = model.gpt.transformer.wte(tokens)
+
+            for i in range(entry_length):
+
+                outputs = model.gpt(inputs_embeds=generated)
+                logits = outputs.logits
+                logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+                sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+                cumulative_probs = torch.cumsum(nnf.softmax(sorted_logits, dim=-1), dim=-1)
+                sorted_indices_to_remove = cumulative_probs > top_p
+                sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[
+                                                    ..., :-1
+                                                    ].clone()
+                sorted_indices_to_remove[..., 0] = 0
+
+                indices_to_remove = sorted_indices[sorted_indices_to_remove]
+                logits[:, indices_to_remove] = filter_value
+                next_token = torch.argmax(logits, -1).unsqueeze(0)
+                next_token_embed = model.gpt.transformer.wte(next_token)
+                if tokens is None:
+                    tokens = next_token
+                else:
+                    tokens = torch.cat((tokens, next_token), dim=1)
+                generated = torch.cat((generated, next_token_embed), dim=1)
+                if stop_token_index == next_token.item():
+                    break
+
+            output_list = list(tokens.squeeze().cpu().numpy())
+            output_text = tokenizer.decode(output_list)
+            generated_list.append(output_text)
+
+    return generated_list[0]
+
+
+# keyword extraction from captions
+def get_hotwords(text):
+    result = []
+    pos_tag = ['NOUN'] # 1
+    doc = nlp(text.lower()) # 2
+    for token in doc:
+        # 3
+        if(token.text in nlp.Defaults.stop_words or token.text in punctuation):
+            continue
+        # 4
+        if(token.pos_ in pos_tag):
+            result.append(token.text)
+                
+    return result # 5
+
+
+is_gpu = True
+device = CUDA(1) if is_gpu else "cpu"
+clip_model, preprocess = clip.load("ViT-B/32", device=device, jit=False)
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+prefix_length = 10
+
+model = ClipCaptionModel(prefix_length)
+model.load_state_dict(torch.load(model_path, map_location=CPU)) 
+
+model = model.eval() 
+device = CUDA(1) if is_gpu else "cpu"
+model = model.to(device)
+
+
+with open(DIR+'metadata.csv','r') as f:
+    meta = f.readlines()
+meta = meta[1:]
+names = [l.split(',')[1] for l in meta]
+
+
+# read in metadata.csv
+# for each line/image, generate caption and update text
+out = []
+
+for file in names:
+    image = io.imread(DIR+file)
+    pil_image = PIL.Image.fromarray(image)
+
+    image = preprocess(pil_image).unsqueeze(0).to(device)
+    with torch.no_grad():
+        prefix = clip_model.encode_image(image).to(device, dtype=torch.float32)
+        prefix_embed = model.clip_project(prefix).reshape(1, prefix_length, -1)
+
+    caption = generate(model, tokenizer, embed=prefix_embed)
+
+    # extract keywords
+    keywords = get_hotwords(caption)
+
+    arr = [caption]+keywords
+    out.append(','.join(arr)+'\n')
+
+# write out
+with open('words.csv', 'w') as f:
+    f.write('caption,keywords...\n')
+    f.writelines(out)
